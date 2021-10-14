6b1bd7c55ff14a738972e3aea6e891646ef94996
Arushi Katyal <katy.arushi@gmail.com>
2021-10-14 01:53:47 -0400

    select queries progress

---
+++ 1_queries/02_avg_length_of_reservation.sql
@@ -0,0 +1,6 @@
+-- Get the average duration of all reservations.
+
+SELECT
+  avg(end_date - start_date) AS average_duration
+FROM
+  reservations;
---
+++ 1_queries/03_property_listings_by_city.sql
@@ -0,0 +1,9 @@
+-- Show all details about properties located in Vancouver including their average rating.
+
+-- Select all columns from the properties table for properties located in Vancouver, and the average rating for each property.
+-- Order the results from lowest cost_per_night to highest cost_per_night.
+-- Limit the number of results to 10.
+-- Only show listings that have a rating >= 4 stars.
+-- To build this incrementally, you can start by getting all properties without the average rating first.
+
+SELECT * FROM properties WHERE city = 'Vancouver';
