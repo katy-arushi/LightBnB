3321c9cd8bc7fe7e205a946b7979240f08b52a48
Arushi Katyal <katy.arushi@gmail.com>
2021-10-14 15:07:41 -0400

    finish select queries

--- 1_queries/03_property_listings_by_city.sql
+++ 1_queries/03_property_listings_by_city.sql
@@ -6,11 +6,19 @@
 -- Only show listings that have a rating >= 4 stars.
 -- To build this incrementally, you can start by getting all properties without the average rating first.
 
-SELECT properties.*, AVG(property_reviews.rating) AS avg_rating
-FROM properties 
-JOIN property_reviews ON properties.id = property_id
-WHERE city LIKE '%ancouv%'
-GROUP BY properties.id
-HAVING avg(property_reviews.rating) >= 4
-ORDER BY cost_per_night
-LIMIT 10;
+SELECT
+  properties.*,
+  AVG(property_reviews.rating) AS avg_rating
+FROM
+  properties
+  JOIN property_reviews ON properties.id = property_id
+WHERE
+  city LIKE '%ancouv%'
+GROUP BY
+  properties.id
+HAVING
+  avg(property_reviews.rating) >= 4
+ORDER BY
+  cost_per_night
+LIMIT
+  10;
