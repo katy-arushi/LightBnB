580a5f1c77afa58e8e35d8dedd1cc806d2807971
Arushi Katyal <katy.arushi@gmail.com>
2021-12-21 00:48:45 -0500

    rollback to e1e9332 commit to fix major bug

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -18,17 +18,16 @@ const users = require('./json/users.json');
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  return pool
-    .query(
-      `SELECT * FROM users 
-     WHERE email = $1`
-      , [email.toLowerCase()])
-    .then((result) => {
-      return result.rows[0];
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
+  let user;
+  for (const userId in users) {
+    user = users[userId];
+    if (user.email.toLowerCase() === email.toLowerCase()) {
+      break;
+    } else {
+      user = null;
+    }
+  }
+  return Promise.resolve(user);
 };
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -39,17 +38,7 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return pool
-    .query(
-      `SELECT * FROM users 
-     WHERE id = $1`
-      , [id])
-    .then((result) => {
-      return result.rows[0];
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
+  return Promise.resolve(users[id]);
 };
 exports.getUserWithId = getUserWithId;
 
@@ -60,18 +49,10 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function(user) {
-  return pool
-    .query(`
-    INSERT INTO users(name, email, password)
-    VALUES ($1, $2, $3)
-    RETURNING *;`
-    , [user.name, user.email, user.password])
-    .then((result) => {
-      return result.rows[0];
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
+  const userId = Object.keys(users).length + 1;
+  user.id = userId;
+  users[userId] = user;
+  return Promise.resolve(user);
 };
 exports.addUser = addUser;
 
@@ -102,6 +83,7 @@ const getAllProperties = (options, limit = 10) => {
   return pool
     .query(`SELECT * FROM properties LIMIT $1`, [limit])
     .then((result) => {
+      console.log(result.rows);
       return result.rows;
     })
     .catch((err) => {
