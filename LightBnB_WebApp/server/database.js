6cf7b6ddde8284da70192efad7f9c4622c32d4e3
Arushi Katyal <katy.arushi@gmail.com>
2022-01-09 13:15:55 -0500

    fix my listings bug

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -133,7 +133,7 @@ const getAllProperties = (options, limit = 10) => {
   let queryString = `
    SELECT properties.*, avg(property_reviews.rating) as average_rating
    FROM properties
-   JOIN property_reviews ON properties.id = property_id
+   LEFT JOIN property_reviews ON properties.id = property_id
    `;
 
   // 3
